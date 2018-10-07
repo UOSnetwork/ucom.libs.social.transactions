129adc0e7fadb9e99a5c843be8e8c3983b53f7b1
vladimir <mr.vladimir.ice@gmail.com>
2018-10-07 23:29:20 +0300

    add dictionary to the package index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = {
-  TransactionFactory:     require('./lib/transaction-factory'),
-  TransactionSender:      require('./lib/transaction-sender'),
-  ContentTypeDictionary:  require('./lib/dictionary/content-type-dictionary')
+  TransactionFactory:         require('./lib/transaction-factory'),
+  TransactionSender:          require('./lib/transaction-sender'),
+
+  ContentTypeDictionary:      require('./lib/dictionary/content-type-dictionary'),
+  InteractionTypeDictionary:  require('./lib/dictionary/interaction-types-dictionary')
 };
